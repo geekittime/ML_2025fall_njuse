55366145096f640d703e03e4a406b4dede4d4c61
czm23333 <34388355+czm23333@users.noreply.github.com>
2025-09-15 17:16:03 +0800

    Reduce the number of used features

--- ML-lab1/feature_extract.py
+++ ML-lab1/feature_extract.py
@@ -5,17 +5,13 @@ if __name__ == "__main__":
     pr_info = pd.read_excel(f'{data_folder}/PR_info_add_conversation.xlsx')
     pr_features = pd.read_excel(f'{data_folder}/PR_features.xlsx')
     author_features = pd.read_excel(f'{data_folder}/author_features.xlsx')
-    pr_commits = pd.read_excel(f'{data_folder}/PR_commit_info.xlsx')
     pr_comments = pd.read_excel(f'{data_folder}/PR_comment_info.xlsx')
 
     # Filter closed pull requests
     pr_info = pr_info[pr_info['state'] == 'closed']
 
     # Delete useless columns
-    del pr_info['author']
-    del pr_info['state']
-    del pr_info['title']
-    del pr_info['body']
+    pr_info = pr_info[["number", "created_at", "updated_at", "merged_at", "closed_at", "merged", "additions", "deletions"]]
 
     # Canonicalize datetime columns
     datetime_cols = ['created_at', 'updated_at', 'merged_at', 'closed_at']
@@ -26,29 +22,13 @@ if __name__ == "__main__":
     # Extract last pr update time
     pr_info['last_pr_update'] = (pr_info['updated_at'] - pr_info['created_at']).dt.total_seconds() / 3600;
 
-    # Extract commit max and avg changes
-    pr_with_commits = pd.merge(pr_info, pr_commits, left_on='number', right_on='belong_to_PR', how='left').groupby(
-        'number')
-    pr_commit_max = pr_with_commits[['changes', 'additions_y', 'deletions_y', 'segs', 'add_segs', 'del_segs']].max()
-    pr_commit_max = pr_commit_max.rename(
-        columns={'changes': 'max_changes', 'additions_y': 'max_additions', 'deletions_y': 'max_deletions',
-                 'segs': 'max_segs', 'add_segs': 'max_add_segs', 'del_segs': 'max_del_segs'})
-    pr_commit_avg = pr_with_commits[['changes', 'additions_y', 'deletions_y', 'segs', 'add_segs', 'del_segs']].mean()
-    pr_commit_avg = pr_commit_avg.rename(
-        columns={'changes': 'avg_changes', 'additions_y': 'avg_additions', 'deletions_y': 'avg_deletions',
-                 'segs': 'avg_segs', 'add_segs': 'avg_add_segs', 'del_segs': 'avg_del_segs'})
-    pr_info = pr_info.merge(pr_commit_max, on='number')
-    pr_info = pr_info.merge(pr_commit_avg, on='number')
-
     # Extract useful features from pr_features
     pr_features = pr_features[
-        ['number', 'has_test', 'has_feature', 'has_bug', 'has_document', 'has_improve', 'has_refactor', 'title_length',
-         'body_length', 'segs_added', 'segs_deleted', 'segs_updated',
-         'files_added', 'files_deleted', 'files_updated', 'comment_length']]
+        ['number', 'title_length', 'body_length', 'files_added', 'files_deleted', 'files_updated']]
     pr_info = pr_info.merge(pr_features, on='number')
 
     # Extract useful features from author_features
-    author_features = author_features[['number', 'participation', 'changes_per_week', 'merge_proportion']]
+    author_features = author_features[['number', 'changes_per_week', 'merge_proportion']]
     pr_info = pr_info.merge(author_features, on='number')
 
     # Extract last comment update time
